d31499f9a1dfe2846a8e5a435ed29d4dd67176f5
Marco R <mprizzuto88@gmail.com>
2021-12-08 21:15:29 -0500

    add email link, update typography

--- index.php
+++ index.php
@@ -35,6 +35,8 @@
               <p class="lfps"><span class="greet">Hello!</span>My name is Marco Rizzuto. I bring a neurodiverse approach to web development  and User Experience.</p>
 
               <p>I am excited to take part in the current web revolution to build highly performant and accessible websites.</p>
+
+              <p>To see my projects, click a shape below. To email me, click <a href="mailto:mprizzuto88@gmail.com" target="_external">here</a></p>
             </div>
 
             <shapes>

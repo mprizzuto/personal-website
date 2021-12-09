d6714d19d4f2f1ccd757ecbaf57f7a3fed3a2d54
Marco R <mprizzuto88@gmail.com>
2021-12-08 21:11:55 -0500

    update header. add SVGs

--- header.php
+++ header.php
@@ -10,10 +10,15 @@
 
 <header class="author-welcome">
   <inner-column>
-    <div class="credentials">
-
+    <div class="dev-info">
       <section class="nav">
-        <h1 class="heading-one">Marco Rizzuto <span class="job">web designer</span></h1>
+        <h1 class="heading-one">Marco Rizzuto</h1>
+
+        <div class="credentials">
+          <?php include "svgs/arrow.svg"; ?>
+          <span class="job">Web Developer</span>
+          
+        </div>
 
         <nav class="site-menu">
           <a class="link" href="index.php">home</a>

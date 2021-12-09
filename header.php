9aabcae3247d77df90248229eb7fd10094bb7b68
Marco R <mprizzuto88@gmail.com>
2021-12-08 20:59:21 -0500

    update site menu

--- header.php
+++ header.php
@@ -17,11 +17,11 @@
 
         <nav class="site-menu">
           <a class="link" href="index.php">home</a>
-          <a class="link" href="experimental.php">experimental</a>
-          <a class="link" href="writing.php">writing</a>
           <a class="link" href="projects.php">projects</a>
-          <a class="link" href="contact.php">contact</a>
-          <a class="link" href="style-guide.php">style guide</a>
+          <a class="link" href="about.php">about</a>
+          <a class="link" href="writing.php">writing</a>
+          
+         
 
           </nav>
       </section>

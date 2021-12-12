3052b7cefa93f3b005fd0f700d01cb2bfdaffeea
Marco R <mprizzuto88@gmail.com>
2021-12-12 17:08:39 -0500

    remove inline styles, create 404 component

--- pages/404.php
+++ pages/404.php
@@ -1,4 +1,13 @@
 
 
 
- <mark style="background: yellow">page not found</mark>
+ <section class="four-o-four">
+  <h2 class="heading-two">
+    <mark>page not found</mark>
+  </h2>
+
+
+   
+  <?php include "./svgs/404-shape.php";?>
+  <p class="warning">Double check that you typed the correct address, or clicked the correct link</p>
+ </section>

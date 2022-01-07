a00f0f76f1a7af34d842b8e1669dc47d29bba46b
Marco R <mprizzuto88@gmail.com>
2022-01-06 21:37:22 -0500

    delete experiment details folder

--- experiment-details/experiment-details-card.php
+++
@@ -1,7 +0,0 @@
-experiment-details-card.php
-<?php include "experiment-details-list.php";?>
-<?php foreach($experimentalDetailsList as $detail) :?>
-<experiment-details-card>
-	<h3><?=$detail[0]["title"]?></h3>
-</experiment-details-card>
-<?php endforeach; ?>

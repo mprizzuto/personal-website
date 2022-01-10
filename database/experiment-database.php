8ea635974f9cffd492c9c4ee67486169868499fc
Marco R <mprizzuto88@gmail.com>
2022-01-09 20:19:53 -0500

    fix query strings for project details page

--- database/experiment-database.php
+++ database/experiment-database.php
@@ -1,35 +1,30 @@
+experimentdatabase.php
 <?php 
-
+// var_dump($_GET);
 $experimentList = [
 	[
 		"id"=> 0,
 		"name" => "jan tschichold poster",
 		"purpose" => "using HTML and CSS to create a cool poster",
-		"link" =>  "experiment-details/?experiment-detail=0",
+		"link" =>  "?page=experiment-detail&experiment=art-poster",
 	],
 	[
 		"id"=> 1,
 		"name" => "SVG and CSS grid study",
 		"purpose" => "using SVG and CSS grid to create poster art ",
-		"link" => "?experiment-detail=1",
+		"link" => "?page=experiment-detail&experiment=svg-grid-study",
 	],
 	[
 		"id"=> 2,
 		"name" => "Tourette symptom checker",
 		"purpose" => "a project that demonstrates form handling. Using PHP",
-		"link" => "?experiment-detail=link-to-detail",
+		"link" => "?page=experiment-detail&experiment=ts-symptom-checker",
 	],
 	[
 		"id"=> 3,
 		"name" => "rotation/transform study",
 		"purpose" => "practice using CSS rotate/transform",
-		"link" => "?experiment-detail=link-to-detail",
-	],
-	[
-		"id"=> 4,
-		"name" => "color study",
-		"purpose" => "a study that uses colors and shapes to evoke feeling.",
-		"link" => "?experiment-detail=link-to-detail",
+		"link" => "?page=experiment-detail&experiment=transform-rotate",
 	],
 ];
 

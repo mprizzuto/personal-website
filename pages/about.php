eaa93c887a7c5fd316c760b8212928cd1d377d21
Marco R <mprizzuto88@gmail.com>
2022-01-12 21:32:08 -0500

    update about data structures

--- pages/about.php
+++ pages/about.php
@@ -1,6 +1,7 @@
-<?php echo "about.php";?>
 <section>
-			<inner-column>
-			<p>the about pagae</p>
-			</inner-column>
-		</section>
+	<inner-column>
+		<?php
+require"./database/about-database.php";
+?>
+	</inner-column>
+</section>
--- pages/writing.php
+++ pages/writing.php
@@ -1,7 +1,9 @@
-<p>the writing pge</p>
-
-<section>
+<section class="blog-posts">
 	<inner-column>
+		<h2>the writing pge</h2>
+
+		<p>My blog Marco’s design journey is all about my unique journey to learn web development as a neurodiverse student. As a Perpetual Education student, I have been exposed to all parts of the design process. I have documented some of it over on my blog. Make sure to subscribe to follow along with my journey</p>
+
 		<?php include "database/writing-database.php";?>
 	</inner-column>
 </section>

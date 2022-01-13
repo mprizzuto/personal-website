fb0ac83ef43322dd54d6139f1a2ec700e2bb3cfe
Marco R <mprizzuto88@gmail.com>
2022-01-12 21:33:43 -0500

    create writing-card component

---
+++ components/writing-card.php
@@ -0,0 +1,9 @@
+writing-card.php
+<section class="writing">
+	<inner-column>
+		<writing-card>
+			<?php include "./images/svgs/left-quote.svg"; ?>
+			<?php include "./images/svgs/right-quote.svg"; ?>
+		</writing-card>
+	</inner-column>
+</section>

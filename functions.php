5b0c3d7e9fef02f9cb09193f134f0fe1d35d5ecc
Marco R <mprizzuto88@gmail.com>
2022-02-23 19:51:58 -0500

    add getHead function to functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php 
+//get the current file .. i.e index.php
+$currentFile = basename($_SERVER["SCRIPT_NAME"]);
+
 // a better var_dump
 function formatVar(mixed $input) {
 	echo "<pre>";
@@ -6,11 +9,13 @@ function formatVar(mixed $input) {
 	echo "</pre>";
 }
 
+//get any page with ?page query string
 function getPage() {
 	$page = $_GET["page"] ?? null;
 	return $page;
 }
 
+//get any page with ?writing query string
 function getWritingPage() {
 	$page = $_GET["writing"] ?? null;
 	return $page;
@@ -24,8 +29,8 @@ function checkPages($page) {
 	// return "true" if page exists, otherwise return "false"
 	return in_array($page, $pageList) ? "true" : "false";
 }
-
 ?>
+
 <?php function cardBuilder($name, $goal, $link) {?>
 <card class="project-card">
 	<details>
@@ -79,23 +84,41 @@ function generateMeta($title, $description, $image) {?>
 <?php }?>
 
 <?php 
-  function getHead () {
+  function getHead() {
   	switch (getPage()) {
-  		case "":
-  			// code...
+  		case "home":
+  			generateMeta("projects", "I am a Web Designer who creates websites that help Health Professionals build and promote their personal brand. Unlike my competitors, I have experience with product beta-testing, and a passion for creating positive change in America's healthcare system.", "htps://peprojects.dev/alpha-1/mprizzuto/personal-website-v2");
   			break;
 
-  		case "":
-  			// code...
+  		case "projects":
+  			generateMeta("projects", "come see my projects", "htps://peprojects.dev/alpha-1/mprizzuto/personal-website-v2");
   			break;
 
-  		
-  		default:
-  			// code...
-  			break;
+  			case "about":
+	  			generateMeta("about", "come read about the man behind the code", "htps://peprojects.dev/alpha-1/mprizzuto/personal-website-v2");
+	  			break;
+
+  			case "writing":
+	  			generateMeta("writing", "like reading? you'll love my blog posts", "htps://peprojects.dev/alpha-1/mprizzuto/personal-website-v2");
+	  			break;
+		
+	  		default:
+					generateMeta("404", "looks like you took a wrong turn", "htps://peprojects.dev/alpha-1/mprizzuto/personal-website-v2");
   	}
   }
 
+	//OR
+  // function getHead() {
+  // 	switch ($_SERVER["SCRIPT_NAME"]) {
+  // 		case 'value':
+  // 			// code...
+  // 			break;
+  		
+  // 		default:
+  // 			// code...
+  // 			break;
+  // 	}
+  // }
 ?>
 
 

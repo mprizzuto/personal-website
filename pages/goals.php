e31874a605dce08b4a5c8064cd3b1d9f447b48f3
Marco R <mprizzuto88@gmail.com>
2022-02-28 19:16:17 -0500

    format goals.php

---
+++ pages/goals.php
@@ -0,0 +1,9 @@
+<?php 
+require "./data/goal-data.php";
+// require_once "./functions.php";
+?>
+<section class="goal-data">
+	<inner-column>
+	  <?php generateGoals($goalData)?>
+	</inner-column>
+</section>

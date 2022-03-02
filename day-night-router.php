78f797e816c9883dc453d68d6ca735543a1dad68
Marco R <mprizzuto88@gmail.com>
2022-03-01 21:07:52 -0500

    change time for time change

--- day-night-router.php
+++ day-night-router.php
@@ -2,17 +2,14 @@
 // $timezone = date_default_timezone_set("America/New_York");
 
 $time = date("H");
-// $timeChange = 13;
 
-// var_dump($time);
+switch ($time) {
+	case $time >= 16 || $time <= 07:
+		include "images/svgs/moon.php";
+		break;
 
-	switch ($time) {
-		case $time >= 16 || $time <= 07:
-			include "images/svgs/moon.php";
-			break;
-
-		default:
-			include "images/svgs/sun.php";
-	}
+	default:
+		include "images/svgs/sun.php";
+}
 
 ?>

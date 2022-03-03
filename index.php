a3dd6e74579e86c1d2791d27c99a51dc8cfc0495
Marco R <mprizzuto88@gmail.com>
2022-03-02 21:55:42 -0500

    echo class on main element based on query string

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
  <?php require "modules/header.php"?>
-    <main class="dev-main">
+    <main class="<?=$queryString?>-main">
       <article class="dev-article">
         <?php 
         require "page-router.php";
         ?>
-
+        <?=var_dump($_GET)?>
       </article>
     </main>
     <?php require "modules/footer.php";

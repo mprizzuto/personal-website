fbe000874b1fad99e760f766e4ac9000d466c191
Marco R <mprizzuto88@gmail.com>
2022-03-09 21:51:32 -0500

    update file paths for responsive layout project

---
+++ projects/responsive-layout/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang='en'>
+	<?php include('./projects/responsive-layout/head.php'); ?>
+
+	<body>
+		<header class='site-header'>
+		<inner-column>
+
+			<?php include('./projects/responsive-layout/modules/masthead.php'); ?>
+
+		</inner-column>
+		</header>
+
+
+		<main class='page-content'>
+			<!-- STUFF -->

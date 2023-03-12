b323231b7e33ec27c9b5e8f2249a66a4f6794079
Marco R <mprizzuto88@gmail.com>
2023-03-12 19:51:07 -0400

    Delete project.php

--- pages/project.php
+++ pages/project.php
@@ -1,24 +1,2 @@
-<?php 
-//get project data
-$project = getProjectBySlug($_GET["slug"]);
-// formatVar($project);
-//show projects
-?>
-
-<h1><?=$project["title"]?></h1>
-<p><?=$project["description"]?></p>
-
-<?php //include the project based on the query string
-switch ($_GET["slug"]) {
-	case "responsive-layout":
-		// echo "responsive latout";
-	include "./projects/responsive-layout/index.php";
-		break;
-	
-	default:
-		echo "no project chosen";
-}
-?>
-
 
 

430b6e10259bf40f11d721f1408d1a3dbd34c30b
Marco R <mprizzuto88@gmail.com>
2023-03-12 19:49:21 -0400

    Style anchors based on current page

--- database/nav-data.php
+++ database/nav-data.php
@@ -3,9 +3,5 @@ $navLinks = [
 	"home" => "?page=home",
 	"projects" => "?page=projects",
 	"about" => "?page=about",
-	"writing" => "?page=writing",
-	"goals" => "?page=goals",
-	"resume" => "?page=resume",
-	"experiments" => "?page=experiments",
 ];
 ?>
--- database/project.php
+++ database/project.php
@@ -7,7 +7,7 @@ $project = getProjectBySlug($_GET["slug"]);
 //show projects
 ?>
 
-<h1><?=$project["title"]?></h1>
+<h1><?=$project["title"] ?? null ?></h1>
 
 
 

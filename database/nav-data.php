eaa594a44c579032c4f08232d3f12f6c8f2f5c50
Marco R <mprizzuto88@gmail.com>
2023-03-13 02:39:29 -0400

    Update nav-data, create site-map data

--- database/nav-data.php
+++ database/nav-data.php
@@ -3,5 +3,13 @@ $navLinks = [
 	"home" => "?page=home",
 	"projects" => "?page=projects",
 	"about" => "?page=about",
+  "experiments" => "?page=experiments",
+
+  "about" => "?page=about",
+  "writing" => "?page=writing",
+  "resume" => "?page=resume",
+  "goals" => "?page=goals",
+  "contact" => "?page=contact",
+
 ];
 ?>
---
+++ database/site-map-data.php
@@ -0,0 +1,11 @@
+<?php 
+$siteData = [
+	"home" => ["home", "projects", "about"],
+	// include an array of actual projects used to generate the projects? so they are always up to date?
+	"projects" => [
+		"project Detail" => ["project detail one", "project detail two"],
+		"experiments" => ["experiment detail one", "experiment detail two"],
+	],
+	"about" => ["about", "writing", "resume", "goals", "contact"],
+];
+?>

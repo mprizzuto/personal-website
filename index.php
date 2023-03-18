5361736d5529cea36843e5fec1a97000cf09039e
Marco R <mprizzuto88@gmail.com>
2023-03-18 03:02:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
- <?php require "modules/header.php"?>
-    <main class="<?=$queryString?>-main">
-      <article class="dev-article">
-        <?php 
-        require "page-router.php";
-        
-        ?>
-      </article>
-    </main>
-    <?php require "modules/footer.php";
+<?php require "modules/header.php"?>
+
+<main class="<?=$queryString?>-main">
+  <article class="dev-article">
+    <h1>Hello!</h1>
+    <?php 
+    require "page-router.php";
+    
+    ?>
+  </article>
+</main>
+
+<?php require "modules/footer.php";

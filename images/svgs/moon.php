86cf35d56f14c74409e78ac3bc345a0c2e0fbe9e
Marco R <mprizzuto88@gmail.com>
2023-03-18 03:01:42 -0400

    Update files

--- images/svgs/moon.php
+++ images/svgs/moon.php
@@ -1,4 +1,4 @@
-<a class="projects-svg" href="?page=projects">
+<a class="projects-svg" href="?page=projects"><?=$projectsCTA?>
     <svg width="100%" height="100%" viewBox="0 0 124 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
       <g transform="matrix(1,0,0,1,-2677.61,-2457.4)">
           <g transform="matrix(0.997769,0,0,1,3007.34,1171.07)">

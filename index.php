ee8cfdbe92be7d874e627f2cb381e19faccb4527
Marco R <mprizzuto88@gmail.com>
2023-03-19 01:08:09 -0400

    Update Index

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php require "modules/header.php"?>
-
 <main class="<?=$queryString?>-main">
-  <article class="dev-article">
-    <h1>Hello!</h1>
+  <article class="<?=$queryString?>-article">
+    <header class="article-header">
+      <inner-column>
+        <h1><?=generatePageTitle()?></h1>
+      </inner-column>
+    </header>
     <?php 
     require "page-router.php";
     

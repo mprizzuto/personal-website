8a56324d3143ec2233e2c74e6bf6bb7d0b7fe1db
Marco R <mprizzuto88@gmail.com>
2023-04-11 02:24:49 -0400

    Include sitemap in footer

--- modules/footer.php
+++ modules/footer.php
@@ -1,6 +1,7 @@
 		<footer class="site-footer">
 		  <inner-column>
 			  <?php include "components/nav.php";?>
+			  <a href="?page=site-map" <?=styleSitemapLink()?>>site-map</a>
 		  </inner-column>
 		</footer>
 		<script src="script.js"></script>
--- modules/header.php
+++ modules/header.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 
-  <body class="homepage"> 
+  <body class=<?php echo getClassByQuery() . "-page"?>> 
     <header class="author-welcome">
       <inner-column>
         <dev-info class="dev-info">

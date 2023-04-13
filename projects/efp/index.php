2e0e143bce360ff1efcc965c0d854a3c5fcd5d02
Marco R <mprizzuto88@gmail.com>
2023-04-13 00:51:37 -0400

    Create index for efp

---
+++ projects/efp/index.php
@@ -0,0 +1,16 @@
+<h1>exercises for programmers</h1>
+
+<section class="efp" id="efp-php">
+	<inner-column>
+		<h2>EFP: PHP</h2>
+
+		<!-- generateEfpPHP() -->
+	</inner-column>
+</section>
+
+<section class="efp" id="efp-js">
+	<inner-column>
+		<h2>EFP: JavaScript</h2>
+		<!-- generateEfpJS() -->
+	</inner-column>
+</section>

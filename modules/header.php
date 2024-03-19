75e5332a0cadab315c405100593784652120e871
marco rizzuto <mprizzuto88@gmail.com>
2024-03-19 01:19:31 -0400

    Update head metadata. personal website

--- modules/header.php
+++ modules/header.php
@@ -31,17 +31,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 
 <!doctype html>
-
   <html lang="en">
   
   <head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" type="text/css" href="./styles/reset.css">
 
     <link rel="stylesheet" type="text/css" href="./styles/style.css">
-
-    <meta charset="utf-8">
+    
     <?=getHead()?>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
   </head>
 
   <body class= "<?php echo getClassByQuery() . "-page " . $_SESSION["theme"] ?? null; ?>"> 

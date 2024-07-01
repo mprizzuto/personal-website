259e6830fdac816a3f31cbccf6e2c81916b72d0b
marco rizzuto <mprizzuto88@gmail.com>
2024-06-30 23:01:33 -0400

    Persoanl website- increase inner-column width

--- public/assets/includes/components/web-technologies.php
+++ public/assets/includes/components/web-technologies.php
@@ -1,22 +1,26 @@
 <ul class="web-tech-list">
   <li>
     <abbr title="HyperText Markup Language">HTML</abbr>
+    <span>for <em>content</em> and <em>structure</em></span>
   </li>
 
   <li>
     <abbr title="cascading style sheets">CSS</abbr>
+    <span>for <em>presentation</em> and <em>style</em></span>
   </li>
 
   <li>
     <abbr title="JavaScript">JS</abbr>
+    <span>for <em>front-end interactivity</em></span>
   </li>
 
   <li>
     <abbr title="PHP: Personal Homepage">PHP</abbr>
+    <span>for <em>server-side rendering</em> and <em>security</em></span>
   </li>
 
-  <li>
+ <!--  <li>
     <abbr title=""></abbr>
-  </li>
+  </li> -->
 
 </ul>

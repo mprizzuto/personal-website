1d46e3a688f942e13bc55f5ea84249eb3154b521
marco rizzuto <mprizzuto88@gmail.com>
2024-07-06 09:47:43 -0400

    Test branch

--- public/functions/nav-functions.php
+++ public/functions/nav-functions.php
@@ -1,29 +1,29 @@
 <?php
-	function globalNav() {
-		include "./database/nav-data.php";
+function globalNav() {
+	include "./database/nav-data.php";
 
-		$globalLinks = ["home", "about", "projects", "experiments"];
+	$globalLinks = ["home", "about", "projects", "experiments"];
 
-		foreach($navLinks as $navLinkText => $navLink) {
-	    if ( in_array($navLinkText, $globalLinks) ) {
-	    	echo " " . "<a href='$navLink'" . styleAnchorLink($navLinkText) . ">" . $navLinkText . "</a>";
-	    }
-	  }
-	}
-
-
-	function aboutPageNav() {
-		include "database/nav-data.php";
-
-		$aboutLinks = ["about", "writing", "resume", "goals", "contact"];
-
-		echo "<aside class='about-links'>" . "<inner-column>". "<h2>curious about me?</h2>" . "<nav class='about-page-nav'>";
-
-    foreach($navLinks as $navLinkText => $navLink) {
-      if ( in_array($navLinkText, $aboutLinks)) {
-				echo " " . "<a href='$navLink'" . styleAnchorLink($navLinkText) . ">" . $navLinkText . "</a>";
-      }
+	foreach($navLinks as $navLinkText => $navLink) {
+    if ( in_array($navLinkText, $globalLinks) ) {
+    	echo " " . "<a href='$navLink'" . styleAnchorLink($navLinkText) . ">" . $navLinkText . "</a>";
     }
-    echo "</nav>" . "</inner-column>" . "</aside>";
-	}
+  }
+}
+
+
+function aboutPageNav() {
+	include "database/nav-data.php";
+
+	$aboutLinks = ["about", "writing", "resume", "goals", "contact"];
+
+	echo "<aside class='about-links'>" . "<inner-column>" . "<h2>curious about me?</h2>" . "<nav class='about-page-nav'>";
+
+  foreach($navLinks as $navLinkText => $navLink) {
+    if ( in_array($navLinkText, $aboutLinks)) {
+			echo " " . "<a href='$navLink'" . styleAnchorLink($navLinkText) . ">" . $navLinkText . "</a>";
+    }
+  }
+  echo "</nav>" . "</inner-column>" . "</aside>";
+}
 ?>

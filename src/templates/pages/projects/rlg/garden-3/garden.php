8a3d27991c64a723b3996f3766fe2f75ea7ddfba
marco rizzuto <mprizzuto88@gmail.com>
2024-07-21 17:18:56 -0400

    Personal website- garden 3 update HTML

--- src/templates/pages/projects/rlg/garden-3/garden.php
+++ src/templates/pages/projects/rlg/garden-3/garden.php
@@ -1,4 +1,4 @@
-<main class="tsch-site-main">
+<section class="tsch-site-main">
   <header class="tsch-page-header">
     <div class="tsch-bookmark">
       <h1>jan tschichold</h1>
@@ -6,17 +6,19 @@
   </header>
   
   <section class="tsch-history">
-    <div class="tsch-bookmark">
+    <header class="tsch-bookmark">
       <h2>titan of typography</h2>
-    </div>
+    </header>
     
     <div class="tsch-advocate">
       <p>leading advocate for <span class="tsch-timeline">modernist design in the late twenties</span></p>
     </div>
-<!--   this part needs some love.. visually -->
     <ul class="tsch-facts">
       <li>prominent 20th century German typographer</li>
-      <li><span class="tsch-dot">&bull; book designer</span></li>
+      <li class="tsch-accent">
+        <span class="tsch-dot">&bull;</span> 
+        <span class="tsch-job">book designer</span>
+    </li>
       <li class="tsch-sub-fact">
         <svg width="400" height="10">
           <rect width="300" height="20"/>
@@ -25,4 +27,4 @@
       </li>
     </ul>
   </section>
-</main>
+</section>

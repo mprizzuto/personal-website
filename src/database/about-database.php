a5854f0217471dc404a10e07d915be4cca9a0df0
marco rizzuto <mprizzuto88@gmail.com>
2024-07-22 19:40:49 -0400

    Personal website- change about.php title

--- src/database/about-database.php
+++ src/database/about-database.php
@@ -2,7 +2,7 @@
 // $TODO- change current photos to those that match my hobbies
 $aboutData = [
 	[
-		"heading" => "About Marco",
+		"heading" => "My background",
 		"my-story" => "As an original PE beta-tester, I had the privilege to help shape and test some of the early lessons. It was through my relationships with Derek and other PEople that I was able to practice and hone my User Experience and Accessibility skills. As a Neurodiverse (Tourettes) Web Developer and User, I have felt the pains first-hand of learning (online and offline) in a world that isn't built for people like us. <br><br> I’m committed to building a more inclusive web experience. I bring a passion and divergent thought process to whatever project I tackle. <span class='hobby-intro'> When I am not programming websites, I enjoy:</span>",
 		"hobbies" => ["baking bread (usually sourdough)", "photography (urban landscape)", "Exercise (jumping rope/boxing)"],
 	"photos" => ["./assets/images/mr-photos/mobile/flowers.jpg" => "the flowers are blooming", "./assets/images/mr-photos/mobile/bw-shadows.jpg" => "before the world wide web, tree branches were the original network", "./assets/images/mr-photos/mobile/bw-branches.jpg" => "we all stand in the shadow of the those who came before us"],

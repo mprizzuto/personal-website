c8cd139c923dae30547932aa48609962c9b9165a
marco rizzuto <mprizzuto88@gmail.com>
2024-07-22 22:04:45 -0400

    Personal website-current styled link is inline-block

--- src/functions/menu-functions.php
+++ src/functions/menu-functions.php
@@ -2,7 +2,7 @@
 function styleAnchorLink($slug) {
 	// TODO. abstract out $navLinkStyle rule into its own function.
 	$queryString = $_GET["page"] ?? null;
-	$navLinkStyle = " style = 'border-bottom: 1px solid maroon;'";
+	$navLinkStyle = " style = 'display: inline-block; border-bottom: 1px solid maroon;'";
 
 	switch ($slug === getQueryString()) {
 		case "home":

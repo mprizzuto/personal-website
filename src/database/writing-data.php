c9cd099781a6e037e91c0ef20db32b1d84de9e81
marco rizzuto <mprizzuto88@gmail.com>
2024-07-24 12:44:50 -0400

    Personal website-parcel out writingList

---
+++ src/database/writing-data.php
@@ -0,0 +1,18 @@
+<?php
+$writingList = [
+	[
+		"id" => "agile-sprinting",
+		"date" => "july 9th, 2021",
+		"title" => "My first agile sprint",
+		"description" => "my fird time doing an agile sprint",
+		"category" => "project-management",
+	],
+	[
+		"id" => "personal-website-case-study",
+		"date" => "nov 16, 2021",
+		"title" => "personal website case study",
+		"description" => "a case study detaiing my personal website work",
+		"category" => "case-study",
+	],
+];
+?>
--- src/database/writing-database.php
+++ src/database/writing-database.php
@@ -1,21 +1,6 @@
 <?php 
 // var_dump($_GET);
-$writingList = [
-	[
-		"id" => "agile-sprinting",
-		"date" => "july 9th, 2021",
-		"title" => "My first agile sprint",
-		"description" => "my fird time doing an agile sprint",
-		"category" => "project-management",
-	],
-	[
-		"id" => "personal-website-case-study",
-		"date" => "nov 16, 2021",
-		"title" => "personal website case study",
-		"description" => "a case study detaiing my personal website work",
-		"category" => "case-study",
-	],
-];
+@require "writing-data.php";
 
 /*
 "id" => "",

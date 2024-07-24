a1a7d77d415d94a339c56173dfed6e1ce6d1fbfd
marco rizzuto <mprizzuto88@gmail.com>
2024-07-24 11:39:39 -0400

    Personal website- call getAllExperiments. sitemap

--- src/database/site-map-data.php
+++ src/database/site-map-data.php
@@ -3,12 +3,8 @@ $siteData = [
 	"home" => [
     getNavData(),
 	],
-	"projects" => [
-		"projects" => [getAllProjects()],
-		"project detail" => ["detail1", "detail2"],
-		"about" => ["writing", "resume", "goals", "contact"],
-		"experiments" => getAllExperiments(),
-	],
-  "about"
+	"experiments" => [
+    getAllExperiments()
+  ]
 ];
 ?>

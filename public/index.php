c7aa82dc6bafd2a7fa4bd1384f9f4b040be2c9e3
marco rizzuto <mprizzuto88@gmail.com>
2024-07-24 19:22:40 -0400

    Persoanl website- Format index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,4 @@
 <?php require "../src/modules/header.php"?>
-<?php 
-// on first page load, echo homepage class
-// if a query exists, echo getClassByQuery()
-?>
 <main class=<?php echo getClassByQuery() . "-main"?> >
   <article class=<?php echo getClassByQuery() . "-article"?>>
     <header class="article-header">

b220ae85afbf32a858bce4bee508c855e81b03eb
marco rizzuto <mprizzuto88@gmail.com>
2024-07-27 21:45:29 -0400

    Personal website- update 404 shape

--- public/assets/images/svgs/404-shape.php
+++ public/assets/images/svgs/404-shape.php
@@ -4,55 +4,55 @@
             <g id="rising-sun" transform="matrix(21.365,0,0,2.48003,-13464.6,-110.576)">
                 <g transform="matrix(0.259424,0,0,0.303829,132.559,33.2796)">
                     <g>
-                        <ellipse cx="765" cy="390" rx="285" ry="290" style="fill:rgb(154,3,30);"/>
+                        <ellipse class="experimental-brown-accent" cx="765" cy="390" rx="285" ry="290"/>
                     </g>
                 </g>
                 <g transform="matrix(0.340122,0,0,0.440551,-173.235,-306.4)">
                     <g>
-                        <rect x="600" y="1500" width="434.762" height="400" style="fill:rgb(154,3,30);"/>
+                        <rect class="experimental-brown-accent" x="600" y="1500" width="434.762" height="400"/>
                     </g>
                 </g>
                 <g transform="matrix(0.147871,0,0,0.40427,97.3802,2.29627)">
                     <g>
-                        <rect x="200" y="300" width="120" height="680" style="fill:rgb(255,209,102);"/>
+                        <rect class="experimental-gold-accent" x="200" y="300" width="120" height="680"/>
                     </g>
                 </g>
                 <g transform="matrix(1.1295e-17,-0.176221,0.126126,2.5345e-18,55.1066,58.3758)">
                     <g>
-                        <rect x="200" y="300" width="120" height="680" style="fill:rgb(154,3,30);"/>
+                        <rect class="experimental-brown-accent" x="200" y="300" width="120" height="680"/>
                     </g>
                 </g>
                 <g transform="matrix(1.58223e-17,-0.264331,0.126126,-8.00042e-19,281.35,583.513)">
                     <g>
-                        <rect x="200" y="300" width="120" height="680" style="fill:rgb(86,113,148);"/>
+                        <rect class="experimental-moon-accent" x="200" y="300" width="120" height="680"/>
                     </g>
                 </g>
                 <g transform="matrix(0.147871,0,0,0.40427,132.87,2.29627)">
                     <g>
-                        <rect x="200" y="300" width="120" height="680" style="fill:rgb(255,209,102);"/>
+                        <rect class="experimental-gold-accent" x="200" y="300" width="120" height="680"/>
                     </g>
                 </g>
                 <g transform="matrix(0.147871,0,0,0.40427,165.402,2.29627)">
                     <g>
-                        <rect x="200" y="300" width="120" height="680" style="fill:rgb(255,209,102);"/>
+                        <rect class="experimental-gold-accent" x="200" y="300" width="120" height="680"/>
                     </g>
                 </g>
                 <g transform="matrix(0.420454,0,0,0.554131,46.6408,91.8783)">
                     <g id="fence">
                         <g transform="matrix(0.351696,0,0,0.729558,358.559,-161.662)">
-                            <rect x="200" y="300" width="120" height="680" style="fill:rgb(255,209,102);"/>
+                            <rect class="experimental-gold-accent" x="200" y="300" width="120" height="680"/>
                         </g>
                         <g transform="matrix(0.351696,0,0,0.729558,507.543,-161.662)">
-                            <rect x="200" y="300" width="120" height="680" style="fill:rgb(255,209,102);"/>
+                            <rect class="experimental-gold-accent" x="200" y="300" width="120" height="680"/>
                         </g>
                         <g transform="matrix(0.351696,0,0,0.729558,437.204,-161.662)">
-                            <rect x="200" y="300" width="120" height="680" style="fill:rgb(255,209,102);"/>
+                            <rect class="experimental-gold-accent" x="200" y="300" width="120" height="680"/>
                         </g>
                         <g transform="matrix(0.351696,0,0,0.729558,577.883,-161.662)">
-                            <rect x="200" y="300" width="120" height="680" style="fill:rgb(255,209,102);"/>
+                            <rect class="experimental-gold-accent" x="200" y="300" width="120" height="680"/>
                         </g>
                         <g transform="matrix(0.351696,0,0,0.729558,648.222,-161.662)">
-                            <rect x="200" y="300" width="120" height="680" style="fill:rgb(255,209,102);"/>
+                            <rect class="experimental-gold-accent" x="200" y="300" width="120" height="680"/>
                         </g>
                     </g>
                 </g>

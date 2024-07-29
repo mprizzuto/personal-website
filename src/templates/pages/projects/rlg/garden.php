ea46f1a3e45586214ab023bad1453afd39893af9
marco rizzuto <mprizzuto88@gmail.com>
2024-07-29 17:51:47 -0400

    Persoanl website- remove theming challenge files

--- src/templates/pages/projects/rlg/garden.php
+++
@@ -1,5 +0,0 @@
-<?php require "../src/templates/pages/projects/rlg/garden-1/garden.php"; ?>
-
-<?php require "../src/templates/pages/projects/rlg/garden-2/garden.php"; ?>
-
-<?php require "../src/templates/pages/projects/rlg/garden-3/garden.php"; ?>
--- src/templates/pages/projects/theming-challenge/README.md
+++
@@ -1,7 +0,0 @@
-# theming challenge
-
-# Goal
-Given the base files
-- pick an industry of interest (I chose real estate)
-- research your industry, noticing common visual themes and tricks
-- apply it to the base files

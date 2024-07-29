851d6bbbfc3933fd90280b7ceedc819aa8530dde
marco rizzuto <mprizzuto88@gmail.com>
2024-07-29 17:49:13 -0400

    Personal website- add gardens

---
+++ src/projects/rlg/garden.php
@@ -0,0 +1,5 @@
+<?php require "../src/templates/pages/projects/rlg/garden-1/garden.php"; ?>
+
+<?php require "../src/templates/pages/projects/rlg/garden-2/garden.php"; ?>
+
+<?php require "../src/templates/pages/projects/rlg/garden-3/garden.php"; ?>

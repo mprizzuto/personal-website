d5c7f837674178b40bbffec46208fe74ddfba2b4
marco rizzuto <mprizzuto88@gmail.com>
2024-07-29 17:49:21 -0400

    Personal website- format footer

--- src/projects/theming-challenge/footer.php
+++ src/projects/theming-challenge/footer.php
@@ -5,6 +5,8 @@
 
 		<footer class='site-footer'>
 		<inner-column>
+
+			<!-- no headers in footers allowed? -->
 			<div class='what-to-call-it-then'>
 				<h2 class='attention-voice'>This is the site footer and it has a "site-map" module in it.</h2>
 
@@ -15,3 +17,6 @@
 
 		</inner-column>
 		</footer>
+
+	</body>
+</html>

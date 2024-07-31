6e82aa6a111a881432ae582dfdb88288ac27f33e
marco rizzuto <mprizzuto88@gmail.com>
2024-07-30 23:27:09 -0400

    Personal website- add garden 4

---
+++ src/projects/rlg/garden-4/garden.php
@@ -0,0 +1,63 @@
+<architect-poster>
+  <section class="ap-welcome">
+    <header class="ap-header">
+      <h2 class="architect-intro">
+       <span>renowned architect</span>
+       <span class="architect-name">frida escobedo</span> brings a refreshingly unique cultural addition to <span class="ap-location">new york city</span>
+      </h2>
+
+      <p>The quaint residential block of Bergen Street welcomes Taller Frida Escobedo's first ever condominium residence. Driven by neighborhood context, the facade takes inspiration from the quintessential bay-window brownstones adorning the neighborhood, interpreting the geometry and materiality in a contemporary and intentional manner. Comprised of custom hand-made brick modules, a humble yet elegant form, the facade promotes the philosophy of complexity in simplicity.
+  Staggering bricks to bays, the architecture's rhythmic shape transitions pace from activated street to domestic refuge.</p>
+
+      <p>The crafted, natural masonry folds, stacks, and interlocks, to create the porous brick lattice structures seen throughout Bergen; a variety of dynamic conditions that allow for light, shadow, and air to seep through its design. A balance of connection between the in- and outdoors.</p>
+    </header>
+
+    <div class="bergen-photo">
+    </div>
+  </section>
+ 
+  <section class="ap-interior">
+    <h2 class="">Bergen Interior</h2>
+    
+    <ul class="interior-design-list">
+      <li>
+        <figure class="ap-figure">
+          <picture class="interior-pic">
+            <img src="https://peprojects.dev/images/portrait.jpg" alt="">
+          </picture>
+          
+          <figcaption>aesop park slope store designed by taller frida escobedo in 2018</figcaption>
+        </figure>
+      </li>
+      
+      <li>
+        <figure class="ap-figure">
+          <picture class="interior-pic">
+            <img src="https://peprojects.dev/images/portrait.jpg" alt="">
+          </picture>
+          
+          <figcaption>process of selecting raw materials for the exterior brick module</figcaption>
+        </figure>
+      </li>
+      
+      <li>
+        <figure class="ap-figure">
+          <picture class="interior-pic">
+            <img src="https://peprojects.dev/images/portrait.jpg" alt="">
+          </picture>
+          
+          <figcaption>detail of the brick lattice to filter light and air</figcaption>
+        </figure>
+      </li>
+    </ul>
+    
+    <p class="ap-quote">
+      <q>simplicity came in the shape of a module that would give a sense of scale</q>
+      
+      <span class="ap-cites">
+        <span class="ap-cite ap-first-cite">-frida escobedo</span>
+        <span class="ap-cite">taller frida escobedo</span>
+      </span>
+    </p>
+  </section>
+</architect-poster>

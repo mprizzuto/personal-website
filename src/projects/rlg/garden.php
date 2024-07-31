b74706c8555410bc64e1cc920fc1100c5fc42b30
marco rizzuto <mprizzuto88@gmail.com>
2024-07-30 23:27:21 -0400

    Personal website- fix file paths for garden

--- src/projects/rlg/garden.php
+++ src/projects/rlg/garden.php
@@ -1,5 +1,7 @@
-<?php require "../src/templates/pages/projects/rlg/garden-1/garden.php"; ?>
+<?php require "../src/projects/rlg/garden-1/garden.php"; ?>
 
-<?php require "../src/templates/pages/projects/rlg/garden-2/garden.php"; ?>
+<?php require "../src/projects/rlg/garden-2/garden.php"; ?>
 
-<?php require "../src/templates/pages/projects/rlg/garden-3/garden.php"; ?>
+<?php require "../src/projects/rlg/garden-3/garden.php"; ?>
+
+<?php require "../src/projects/rlg/garden-4/garden.php"; ?>

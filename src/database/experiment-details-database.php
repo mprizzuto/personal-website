5d550a0b9b14bed2af735d69df930e0d9a6be312
marco rizzuto <mprizzuto88@gmail.com>
2024-07-31 17:15:40 -0400

    Persoanl website- remove old experiment

--- src/database/experiment-details-database.php
+++ src/database/experiment-details-database.php
@@ -9,10 +9,6 @@ setExperimentDetailTitle(getExperimentName());
   <inner-column>
     <?php
       switch ($experiment) {
-        case "most-a11y-page":
-          experimentDetailBuilder("most-a11y-page", "can i make the most accessible page ever?", "skitch, affinity designer, sublime text", "https://via.placeholder.com/150", "ALT", "the result was, i was successful!");
-          break;
-
         case "ts-symptom-checker":
           experimentDetailBuilder("TS symptom checker", "can i create a form with seamless UX to empower people with TS to report their symptoms?", " affinity designer, sublime text", "https://via.placeholder.com/150", "a figure showing screenshots of my work", "the result was, the users found the form easy to fill out and use!");
           break;
@@ -30,7 +26,7 @@ setExperimentDetailTitle(getExperimentName());
           break;     
         
         // case "":
-        //   experimentDetailBuilder("", "", "", "", "I", "");
+        //   experimentDetailBuilder("name", "hypothesis", "materials", "image", "alt", "result");
         //   break;
 
 

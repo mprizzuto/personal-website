2785856e08e51256b6090ea07722e1670c6ed18a
marco rizzuto <mprizzuto88@gmail.com>
2024-08-01 19:05:29 -0400

    Personal website- remove link from svg

--- public/assets/images/svgs/sun.php
+++ public/assets/images/svgs/sun.php
@@ -1,84 +1,81 @@
-<a class="projects-svg" href="?page=projects">
-    <svg class="sun" width="100%" height="100%" viewBox="0 0 134 100" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
-      <g transform="matrix(1,0,0,1,-2530.89,-2457.4)">
-        <g transform="matrix(0.997769,0,0,1,3007.34,1171.07)">
-            <g transform="matrix(0.992908,0,0,0.97644,-4836.81,-844.163)">
-                <ellipse class="golden-sun" cx="4474.36" cy="2233.1" rx="50.47" ry="51.206"/>
+<svg class="sun" width="100%" height="100%" viewBox="0 0 134 100" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+  <g transform="matrix(1,0,0,1,-2530.89,-2457.4)">
+    <g transform="matrix(0.997769,0,0,1,3007.34,1171.07)">
+        <g transform="matrix(0.992908,0,0,0.97644,-4836.81,-844.163)">
+            <ellipse class="golden-sun" cx="4474.36" cy="2233.1" rx="50.47" ry="51.206"/>
+        </g>
+    </g>
+    <g transform="matrix(0.997769,0,0,1,3007.34,1171.07)">
+        <g id="fence" transform="matrix(1,0,0,1,-97.8807,275.503)">
+            <g transform="matrix(0.9538,0,0,1.06917,-4727.7,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4723.07,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4718.44,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531">
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4713.81,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4709.18,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4704.55,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4699.92,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4695.29,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4690.66,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4686.03,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4681.4,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4676.78,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4672.15,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g transform="matrix(0.9538,0,0,1.06917,-4667.52,-1321.99)">
+                <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+            </g>
+            <g id="second-fence" serif:id="second fence">
+                <g transform="matrix(0.9538,0,0,1.06917,-4662.89,-1321.99)">
+                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+                </g>
+                <g transform="matrix(0.9538,0,0,1.06917,-4658.26,-1321.99)">
+                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+                </g>
+                <g transform="matrix(0.9538,0,0,1.06917,-4653.63,-1321.99)">
+                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+                </g>
+                <g transform="matrix(0.9538,0,0,1.06917,-4649,-1321.99)">
+                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+                </g>
+                <g transform="matrix(0.9538,0,0,1.06917,-4644.37,-1321.99)">
+                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+                </g>
+                <g transform="matrix(0.9538,0,0,1.06917,-4639.74,-1321.99)">
+                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+                </g>
+                <g transform="matrix(0.9538,0,0,1.06917,-4635.11,-1321.99)">
+                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+                </g>
+                <g transform="matrix(0.9538,0,0,1.06917,-4630.48,-1321.99)">
+                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
+                </g>
             </g>
         </g>
-        <g transform="matrix(0.997769,0,0,1,3007.34,1171.07)">
-            <g id="fence" transform="matrix(1,0,0,1,-97.8807,275.503)">
-                <g transform="matrix(0.9538,0,0,1.06917,-4727.7,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4723.07,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4718.44,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531">
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4713.81,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4709.18,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4704.55,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4699.92,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4695.29,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4690.66,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4686.03,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4681.4,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4676.78,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4672.15,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g transform="matrix(0.9538,0,0,1.06917,-4667.52,-1321.99)">
-                    <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                </g>
-                <g id="second-fence" serif:id="second fence">
-                    <g transform="matrix(0.9538,0,0,1.06917,-4662.89,-1321.99)">
-                        <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                    </g>
-                    <g transform="matrix(0.9538,0,0,1.06917,-4658.26,-1321.99)">
-                        <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                    </g>
-                    <g transform="matrix(0.9538,0,0,1.06917,-4653.63,-1321.99)">
-                        <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                    </g>
-                    <g transform="matrix(0.9538,0,0,1.06917,-4649,-1321.99)">
-                        <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                    </g>
-                    <g transform="matrix(0.9538,0,0,1.06917,-4644.37,-1321.99)">
-                        <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                    </g>
-                    <g transform="matrix(0.9538,0,0,1.06917,-4639.74,-1321.99)">
-                        <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                    </g>
-                    <g transform="matrix(0.9538,0,0,1.06917,-4635.11,-1321.99)">
-                        <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                    </g>
-                    <g transform="matrix(0.9538,0,0,1.06917,-4630.48,-1321.99)">
-                        <rect x="4558.67" y="2181.89" width="3.152" height="93.531"/>
-                    </g>
-                </g>
-            </g>
-        </g>
-      </g>
-    </svg>
-
-</a>
+    </g>
+  </g>
+</svg>

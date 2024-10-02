3c44499db3b8fd6594d83f3acb0acf0f6813dc0d
marco rizzuto <mprizzuto88@gmail.com>
2024-10-01 22:05:45 -0400

    Personal website- Update script

---
+++ public/assets/scripts/goals.js
@@ -0,0 +1,42 @@
+var goalsData = {
+	heading: "My amazing goals",
+    sections: [
+		{
+			heading: "End of the week",
+	        goals: [
+				text: "Have my website structure in place",
+	            other: "other...",
+			],
+		},
+		{
+			heading: "End of the week",
+	        goals: [
+				text: "Have my website structure in place",
+	            other: "other...",
+			],
+		},
+		{
+			heading: "End of month",
+	        goals: [
+				text: "Have my website structure in place",
+	            other: "other...",
+			],
+		},
+		{
+			heading: "6 months",
+	        goals: [
+				text: "Have my website structure in place",
+	            other: "other...",
+			],
+		},
+		{
+			heading: "5 years",
+	        goals: [
+				text: "Have my website structure in place",
+	            other: "other...",
+			],
+		},
+	],
+};
+
+console.log(goalsData);

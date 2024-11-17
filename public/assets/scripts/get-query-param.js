ea90bc03faa5136d0d1581d13e7d4f3e5205cbd6
marco rizzuto <mprizzuto88@gmail.com>
2024-11-17 00:40:02 -0500

    conditionally load CSS based on query param

---
+++ public/assets/scripts/get-query-param.js
@@ -0,0 +1,45 @@
+const searchParams = new URLSearchParams(document.location.search);
+
+let pageQuery = searchParams.get("page");
+
+let slugQuery = searchParams.get("slug");
+
+function createCssLink(url, rel) {
+  let link = document.createElement("link");
+
+  link.href = url;
+  link.rel = rel;
+  console.log(url, rel);
+  return link;
+}
+
+function appendCssLinkToHead(url, rel) {
+  // get head
+  // call createCssLink(url, rel)
+  // append to head
+
+  let head = document.querySelector("head");
+
+  let createLink = createCssLink(url, rel);
+  createLink.id = "haha";
+  head.append(createLink);
+}
+
+function renderStyles(pageQuery, slugQuery) {
+  if (pageQuery === "project") {
+    if (slugQuery === "responsive-layout-garden") {
+      appendCssLinkToHead("./assets/styles/project-styles/layout-garden/style.css", "stylesheet");
+    }
+
+    if (slugQuery === "theming-challenge") {
+      appendCssLinkToHead("./assets/styles/project-styles/theming-challenge/css/style.css", "stylesheet");
+    }
+  }
+
+}
+
+renderStyles(pageQuery, slugQuery);
+
+// console.log(typeof pageQuery, pageQuery);
+
+

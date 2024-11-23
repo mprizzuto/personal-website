e7c1c43085a9958d88bda46e8de098aa6d5cd284
marco rizzuto <mprizzuto88@gmail.com>
2024-11-22 22:13:57 -0500

    Update case study template

--- src/templates/pages/case-studies.php
+++ src/templates/pages/case-studies.php
@@ -7,9 +7,16 @@
 
     <?php if ($caseStudy): ?>
       <h2><?=$caseStudy["title"] ?? null?></h2>
-      <p><?=$caseStudy["hypothesis"] ?? null?></p>
-      <p><?=$caseStudy["conclusion"] ?? null?></p>
+      <p><strong>case study:</strong> <?=$caseStudy["conclusion"] ?? null?></p>
       
+      <?php
+      if ( is_array($caseStudy["hypothesis"]) ) {
+        foreach ($caseStudy["hypothesis"] as $hypothesis) {
+          echo "<p>" . $hypothesis . "</p>";
+        }
+      }
+        
+      ?>
       <?php else: ?>
       <p><mark>invalid case study</mark>. check the link and try again</p>
     <?php ENDIF; ?>  

7941d85b8e7a0e9ee764f28abf64d8e883ad6ad8
marco rizzuto <mprizzuto88@gmail.com>
2024-11-23 23:19:10 -0500

    Theming challenge-update masthead template

---
+++ src/projects/theming-challenge/modules/template.php
@@ -0,0 +1,36 @@
+
+<mast-head>
+
+	<!-- <input id='toggle' type='checkbox' /> -->
+
+	<nav class='site-menu'>
+		<ul>
+			<li>
+				<a class='home' href='#'>
+					<?php include('modules/logo.php'); ?>
+				</a>
+			</li>
+
+			<li class='page-link'>
+				<a href='#'>Some page</a>
+			</li>
+
+			<li class='page-link'>
+				<a href='#'>Some other page</a>
+			</li>
+
+			<li>
+				<input id='toggle' type='checkbox' />
+			</li>
+		</ul>
+	</nav>
+
+	<nav class='user-menu'>
+		<ul>
+			<li>
+				<a class='button' href='#'>Sign in</a>
+			</li>
+		</ul>
+	</nav>
+
+</mast-head>
